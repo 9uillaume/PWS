dd25b243dc404c89b796b61a25b43c35886fcf14
Guiz <guillaume.briard@epitech.eu>
2016-04-04 15:46:29 +0200

    view music albums connected with DB

--- music/views.py
+++ music/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
+def index(request):
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums': all_albums,
+    }
+    return render(request, 'music/index.html', context)
+
+def detail(request, album_id):
+    return HttpResponse("<h4>Details of Album id: " + str(album_id) + "</h4>")
