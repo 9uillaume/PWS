e7c316d71d121adcf07f4d7fd2dc8f3f6454715e
Guiz <guillaume.briard@epitech.eu>
2016-04-26 12:57:15 +0200

    user registration and fixes

--- music/models.py
+++ music/models.py
@@ -5,7 +5,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=50)
     album_title = models.CharField(max_length=100)
     genre = models.CharField(max_length=30)
-    album_logo = models.CharField(max_length=1000)
+    album_logo = models.FileField()
 
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk' : self.pk})
@@ -16,8 +16,8 @@ class Album(models.Model):
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=100)
+    audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
--- music/urls.py
+++ music/urls.py
@@ -7,16 +7,18 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/{{pk}}/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name='detail'),
 
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
-    # /music/album/{{pk}/
+    # /music/album/{{pk}}/
     url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
 
-    # /music/album/{{pk}/
+    # /music/album/{{pk}}/delete/
     url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
